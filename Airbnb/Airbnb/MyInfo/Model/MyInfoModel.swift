d93fb7268ff22c7b4c89f354124a26fbb8db6aa4
Damagucci-Juice <jsr9874@gmail.com>
2022-05-23 18:02:14 +0900

    feat: 프로젝트 파일 생성 및 gitignore 설정

---
+++ Airbnb/Airbnb/MyInfo/Model/MyInfoModel.swift
@@ -0,0 +1,8 @@
+//
+//  MyInfoModel.swift
+//  Airbnb
+//
+//  Created by YEONGJIN JANG on 2022/05/23.
+//
+
+import Foundation
---
+++ Airbnb/Airbnb/Search/View/SearchViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchViewController.swift
+//  Airbnb
+//
+//  Created by YEONGJIN JANG on 2022/05/23.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
