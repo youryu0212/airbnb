8d13a01b137b8158100792ae865c3ac0395abc93
TaeKyeongKim <kaikim@Kaiui-MacBookPro.local>
2022-05-24 00:10:12 +0900

    feat: [#5] git ignore 추가

---
+++ Airbnb/Airbnb/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Airbnb
+//
+//  Created by Kai Kim on 2022/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
