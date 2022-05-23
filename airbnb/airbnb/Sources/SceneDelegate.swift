ec7373d1a5881b5820814408c6380ebf9cb917c6
shingha <shingha1124@gmail.com>
2022-05-23 11:43:24 +0900

    프로젝트 초기 설정

---
+++ airbnb/airbnb/Sources/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  airbnb
+//
+//  Created by seongha shin on 2022/05/23.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+    }
+
+}
+
---
+++ airbnb/airbnb/Sources/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  airbnb
+//
+//  Created by seongha shin on 2022/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
