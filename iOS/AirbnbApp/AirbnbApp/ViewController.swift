9fc2545e184d6c471b2ad11852f01c363dd80de0
sungju-kim <wnwn0719@gmail.com>
2022-05-23 19:21:29 +0900

    feat: Airbnb 프로젝트 생성 및 초기설정
    - Airbnb 프로젝트 생성
    - Alamofire 설치
    - Swiftlint 설치
    - 초기 주석제거
    
    Closes #3

---
+++ iOS/AirbnbApp/AirbnbApp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  AirbnbApp
+//
+//  Created by dale on 2022/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
