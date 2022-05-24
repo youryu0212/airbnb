bdd7d34ef1c324fc5ebd021d28f1c2d23bdda4a5
BumgeunSong <isp1195@gmail.com>
2022-05-24 17:22:13 +0900

    Feat: 검색 초기 화면에서 위치 권한 요청 추가 #3

--- iOS/Targets/Cherrybnb/Sources/Search/ViewController/SearchRootViewController.swift
+++ iOS/Targets/Cherrybnb/Sources/Search/ViewController/SearchRootViewController.swift
@@ -7,16 +7,32 @@
 //
 
 import UIKit
+import CoreLocation
 
 class SearchRootViewController: UIViewController {
 
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureNavBar()
+        askPermission()
     }
     
     private func configureNavBar() {
         navigationController?.navigationBar.isHidden = true
     }
+    
+    private func askPermission() {
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.startUpdatingLocation()
+    }
+}
 
+extension SearchRootViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // TODO: Handling updated location
+    }
 }
