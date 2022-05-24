12a40044535995f9dddce3b71312b426d2f9017b
jm <jm.dev.study@gmail.com>
2022-05-24 14:51:29 +0900

    Refact: 라우터와 관련된 컴포넌트를 파일로 분리
    Co-authored-by: herrakam <herrakam@users.noreply.github.com>

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from 'styles/GlobalStyles';
 import { theme } from 'styles/theme';
 
-import Layout from 'pages/Layout';
-import Home from 'pages/Home';
-import Result from 'pages/Result';
+import Router from 'Router';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="result" element={<Result />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Router />
     </ThemeProvider>
   );
 }
