06f5d60587e050e691db10fa4b1c59a506d9df89
jm <jm.dev.study@gmail.com>
2022-05-24 14:45:34 +0900

    Feat: 라우터 적용
    Co-authored-by: herrakam <herrakam@users.noreply.github.com>

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from 'styles/GlobalStyles';
 import { theme } from 'styles/theme';
 
+import Layout from 'pages/Layout';
+import Home from 'pages/Home';
+import Result from 'pages/Result';
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyles></GlobalStyles>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="result" element={<Result />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
---
+++ fe/src/pages/Result.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Result() {
+  return <div>result</div>;
+}
+
+export default Result;
