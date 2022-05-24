1cd5e3ec9f0f98fc24c3453a29f0bf14cd1e8758
bibi <bibi6666667@kakao.com>
2022-05-24 17:28:58 +0900

    [#2] Feat: searchBar 추가

--- airbnb/airbnb/ViewController/MainTabBarController.swift
+++ airbnb/airbnb/ViewController/MainTabBarController.swift
@@ -2,22 +2,27 @@ import UIKit
 
 class MainTabBarController: UITabBarController {
     
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+    
         
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-              
-        let searchTabVC = SearchViewController()
+
+        let searchTabVC = UINavigationController(rootViewController: SearchViewController())
+        
         let searchTabBarItem = UITabBarItem(title: "검색",
                                          image: UIImage(systemName: "magnifyingglass.circle"),
                                          selectedImage: UIImage(systemName: "magnifyingglass.circle.fill"))
         searchTabVC.tabBarItem = searchTabBarItem
-        
+
         self.viewControllers = [searchTabVC]
-        
+
     }
     
 }
