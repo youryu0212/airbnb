24ea4de801ae8b0ed5512b0b8d3d9e6697052c8b
asqw887 <asqw887@gmail.com>
2022-05-24 14:48:11 +0900

    Fix: 프로젝트 Sources 폴더 재구성

---
+++ iOS/Targets/Cherrybnb/Sources/Search/View/SearchView.swift
@@ -0,0 +1,21 @@
+//
+//  SearchView.swift
+//  Cherrybnb
+//
+//  Created by 최예주 on 2022/05/24.
+//  Copyright © 2022 Codesquad. All rights reserved.
+//
+
+import UIKit
+
+class SearchView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ iOS/Targets/Cherrybnb/Sources/Search/ViewController/SearchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchViewController.swift
+//  Cherrybnb
+//
+//  Created by 최예주 on 2022/05/24.
+//  Copyright © 2022 Codesquad. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
