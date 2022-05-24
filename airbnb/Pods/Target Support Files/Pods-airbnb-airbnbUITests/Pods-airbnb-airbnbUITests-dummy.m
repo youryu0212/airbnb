6d6d2afa0ea3bf530130ed365a118d23d850a972
Jihee hwang <hwangjihee94@gmail.com>
2022-05-24 16:05:34 +0900

    #2 chore: 불필요한 폴더 및 파일 삭제

--- airbnb/Pods/Target Support Files/Pods-airbnb-airbnbUITests/Pods-airbnb-airbnbUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_airbnb_airbnbUITests : NSObject
-@end
-@implementation PodsDummy_Pods_airbnb_airbnbUITests
-@end
--- airbnb/Pods/Target Support Files/Pods-airbnbTests/Pods-airbnbTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_airbnbTests : NSObject
-@end
-@implementation PodsDummy_Pods_airbnbTests
-@end
