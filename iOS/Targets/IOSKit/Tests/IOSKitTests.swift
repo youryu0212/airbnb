1c98d52bcd56023d75f38958e5ea8004b6f273d7
asqw887 <asqw887@gmail.com>
2022-05-24 14:15:44 +0900

    chore: 불필요한 파일 삭제

--- iOS/Targets/IOSKit/Tests/IOSKitTests.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-import XCTest
-
-final class IOSKitTests: XCTestCase {
-    func test_example() {
-        XCTAssertEqual("IOSKit", "IOSKit")
-    }
-}
--- iOS/Targets/IOSUI/Sources/IOSUI.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-public final class IOSUI {
-    public static func hello() {
-        print("Hello, from your UI framework")
-    }
-}
