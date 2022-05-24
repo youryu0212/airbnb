bde8711773f384b98ea9d3b6b39407b59ea49689
Ubeom Kim <kimub1204@gmail.com>
2022-05-24 21:16:53 +0900

    refactor: 개발 환경 셋팅 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
-import { styled as muiStyled } from '@mui/system';
-import styled from 'styled-components';
+import { styled } from '@mui/system';
+import { Button } from '@mui/material';
+
+const CustomDiv = styled('div')`
+  width: 50px;
+  height: 50px;
+  background-color: black;
+`;
+
+const CustomButton = styled(Button)`
+  width: 100px;
+  height: 100px;
+`;
 
 function App() {
-  return <div />;
+  return (
+    <div>
+      <CustomDiv />
+      <CustomButton>버튼</CustomButton>
+    </div>
+  );
 }
 
 export default App;
