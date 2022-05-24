208c3262b62695c9847749d13968f4b3d312ac8b
bibi <bibi6666667@kakao.com>
2022-05-24 17:29:44 +0900

    [#2] Feat: searchBar 추가

--- airbnb/airbnb/ViewController/SearchViewController.swift
+++ airbnb/airbnb/ViewController/SearchViewController.swift
@@ -1,11 +1,18 @@
 import UIKit
 
-class SearchViewController: UINavigationController {
+class SearchViewController: UIViewController {
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = .systemBackground
+        
+        let searchBar = UISearchBar()
+
+        searchBar.placeholder = "어디로 여행가세요?"
+        self.navigationItem.titleView = searchBar
+        
+
     }
 }
