c76cfe3254cc2170551fd2cec14d211d9a0b8052
SeungHyun <fm10033@gmail.com>
2022-05-24 11:34:34 +0900

    build : 스타일링 관련 초반 세팅
    material 페키지 설치
    globalStyle 설정
    아이콘, 배경 다운로드

---
+++ fe/src/styles/GlobalStyles.jsx
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyles = createGlobalStyle`
+	${reset};
+	* { 
+		margin: 0; 
+		padding: 0; 
+		box-sizing: border-box; 
+	} 
+	body { 
+		font-family: 'Noto Sans KR', sans-serif; 
+	} 
+	a {
+		text-decoration: none; 
+		color: transparent;
+	}
+	button { 
+		border: none;
+		outline: none; 
+		background: transparent; 
+		cursor: pointer; 
+	}
+
+`;
---
+++ fe/src/styles/theme.js
@@ -0,0 +1,14 @@
+const theme = {
+  colors: {
+    white: '#ffffff',
+    grey6: '#F5F5F7',
+    grey5: '#E0E0E0',
+    grey4: '#BDBDBD',
+    grey3: '#828282',
+    grey2: '#4F4F4F',
+    grey1: '#333333',
+    black: '#1b1b1b',
+    primary: '#E84C60',
+    secondary: '#118917',
+  },
+};
