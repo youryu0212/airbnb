ce94e63f8683403d845d1aa85eca152c9378d381
Senglee Choi <tmdfl1027@gmail.com>
2022-05-24 11:15:45 +0900

    [#3] Feat: 스프링부트 프로젝트 초기 설정

---
+++ be/src/test/java/kr/codesquad/airbnb/AirbnbApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.codesquad.airbnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
