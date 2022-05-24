6e47303a66cc3b8aa0e1c472d0b3c1bb66db3457
junseokseo <junseoks9306@gmail.com>
2022-05-24 19:45:36 +0900

    feat: 힐트 okhttp 모듈 생성 (#8)

---
+++ android/app/src/main/java/com/example/airbnb/MainApplication.kt
@@ -0,0 +1,5 @@
+package DI
+
+import android.app.Application
+
+class Reservation : Application()
---
+++ android/app/src/main/java/com/example/airbnb/di/NetworkModule.kt
@@ -0,0 +1,30 @@
+package com.example.airbnb.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import javax.inject.Named
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    private const val BASE_URL = "https://dbe23652-7fd3-4472-b25a-37d99d31e87e.mock.pstmn.io/api"
+
+    @Provides
+    @Singleton
+    @Named("home")
+    fun provideOkHttpClient(): OkHttpClient {
+        val logger = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        return OkHttpClient.Builder()
+            .addInterceptor(logger)
+            .build()
+    }
+
+}
