71a4bfda02f72541956fd4c4502f362d3e610476
TaeKyeongKim <kaikim@Kaiui-MacBookPro.local>
2022-05-24 13:15:26 +0900

    add: [#5]Icons

--- Airbnb/Airbnb/Screens/Home/View/HomeVC.swift
+++ Airbnb/Airbnb/Screens/Home/View/HomeVC.swift
@@ -11,7 +11,6 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .Primary
+        self.view.backgroundColor = .primary
     }
-
 }
