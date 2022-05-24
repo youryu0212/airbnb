9aef8691e4bd9c3f7dedbaf32568cf286280e4ea
jindonyy <rsefa94@gmail.com>
2022-05-24 14:17:05 +0900

    Design: font, css 공통 변수 설정
    issue #3

---
+++ src/constant/font.ts
@@ -0,0 +1,20 @@
+const FONT = {
+  SIZE: {
+    X_SMALL: '0.75rem', // 12px
+    SMALL: '0.875rem', // 14px
+    BASE: '1rem', // 16px
+    MEDIUM: '1.125rem', // 18px
+    LARGE: '1.25rem', // 20px
+    TITLE_1: '2rem' // 32px
+  },
+  WEIGHT: {
+    REGULAR: 400,
+    MEDIUM: 700,
+    BOLD: 900
+  },
+  FAMILY: {
+    BASE: "'Noto Sans KR', sans-serif"
+  }
+};
+
+export default FONT;
