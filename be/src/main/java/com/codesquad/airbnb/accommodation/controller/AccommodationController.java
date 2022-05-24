e8ef741917514f2df52e6e0ec96d828e7da63596
seyeong <seyoung7555@naver.com>
2022-05-24 11:36:39 +0900

    feat: 숙소 정보 전달을 담당할 controller 초기 설계
    - 숙소 목록 조회
    - 숙소 가격대 목록 조회
    - 숙소 상세 정보 조회

---
+++ be/src/main/java/com/codesquad/airbnb/accommodation/controller/AccommodationController.java
@@ -0,0 +1,41 @@
+package com.codesquad.airbnb.accommodation.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/accommodations")
+@RestController
+public class AccommodationController {
+
+    @GetMapping
+    public String showList() {
+        /**
+         * TO-DO
+         * - Parameter: 숙소 검색 조건
+         * - Return: DB로부터 조회한 숙소 목록을 출력한다.
+          */
+        return null;
+    }
+
+    @GetMapping("/{accommodation_id}")
+    public String showDetail(@PathVariable Long accommodation_id) {
+        /**
+         * TO-DO
+         * - Parameter: 숙소 id
+         * - Return: DB로부터 조회한 해당 숙소를 출력한다.
+         */
+        return null;
+    }
+
+    @GetMapping("/prices")
+    public String showPriceList() {
+        /**
+         * TO-DO
+         * - Parameter: (체크인 날짜, 체크아웃 날짜가 담긴 검색 조건)
+         * - Return: DB로부터 조회한 해당 일정의 숙소 가격 정보를 보낸다.
+         */
+        return null;
+    }
+}
