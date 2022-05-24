19f708121f91316822e853a61b35eddd5e8099be
hanchang97 <nimok97@gmail.com>
2022-05-24 12:08:34 +0900

    init: 프로젝트 생성 및 초기 설정

---
+++ Android/app/src/main/java/com/team16/airbnb/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.team16.airbnb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
