f4d73d63637297cda40579b93f89f01b4449a32c
jindonyy <rsefa94@gmail.com>
2022-05-24 16:16:48 +0900

    Design: layout css 적용
    close issue #3

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 function App() {
-  return <CssBaseline>aa</CssBaseline>;
+  return (
+    <Box
+      sx={{
+        minHeight: '640px',
+        background: 'url(./assets/hero-img.jpg) no-repeat center top / cover'
+      }}
+    >
+      <CssBaseline />
+      <Container maxWidth="lg">content</Container>
+    </Box>
+  );
 }
 
 export default App;
