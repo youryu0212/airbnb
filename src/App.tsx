5045c93abae800be7ea76ea81f8d0ff7969b00dc
jindonyy <rsefa94@gmail.com>
2022-05-24 16:27:58 +0900

    Design: header 생성
    close issue #3

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
+import Header from '@components/Header';
 
 function App() {
   return (
-    <Box
-      sx={{
-        minHeight: '640px',
-        background: 'url(./assets/hero-img.jpg) no-repeat center top / cover'
-      }}
-    >
+    <>
       <CssBaseline />
-      <Container maxWidth="lg">content</Container>
-    </Box>
+      <Header />
+    </>
   );
 }
 
