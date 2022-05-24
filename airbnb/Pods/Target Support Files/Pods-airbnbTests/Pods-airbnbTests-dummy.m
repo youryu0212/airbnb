9fda9646250746686899a95d52d27f4748446529
Jihee hwang <hwangjihee94@gmail.com>
2022-05-24 16:00:12 +0900

    #2 chore: 프로젝트 파일 생성 및 Swift Lint / SnapKit install

---
+++ airbnb/Pods/Target Support Files/Pods-airbnbTests/Pods-airbnbTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_airbnbTests : NSObject
+@end
+@implementation PodsDummy_Pods_airbnbTests
+@end
---
+++ airbnb/airbnb/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  airbnb
+//
+//  Created by Jihee hwang on 2022/05/24.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
