4dfe60dac50b4e09ccab301d03541ca8d4220a44
sanghyeok-kim <slsnsi1212@gmail.com>
2022-05-24 12:41:35 +0900

    chore: 파일 그룹핑 작업
    - Resource, Source
    
    Closes #3

---
+++ iOS/AirbnbApp/AirbnbApp/Source/Extension/UIColor+Extension.swift
@@ -0,0 +1,23 @@
+//
+//  UIColor+Extension.swift
+//  AirbnbApp
+//
+//  Created by 김상혁 on 2022/05/24.
+//
+
+import UIKit.UIColor
+
+extension UIColor {
+    enum Custom {
+        static let white = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        static let gray1 = UIColor(red: 51 / 255, green: 51 / 255, blue: 51 / 255, alpha: 1.0)
+        static let gray2 = UIColor(red: 79 / 255, green: 79 / 255, blue: 79 / 255, alpha: 1.0)
+        static let gray3 = UIColor(red: 130 / 255, green: 130 / 255, blue: 130 / 255, alpha: 1.0)
+        static let gray4 = UIColor(red: 189 / 255, green: 189 / 255, blue: 189 / 255, alpha: 1.0)
+        static let gray5 = UIColor(red: 224 / 255, green: 224 / 255, blue: 224 / 255, alpha: 1.0)
+        static let gray6 = UIColor(red: 245 / 255, green: 245 / 255, blue: 247 / 255, alpha: 1.0)
+        static let black = UIColor(red: 1 / 255, green: 1 / 255, blue: 1 / 255, alpha: 1.0)
+        static let hotPink = UIColor(red: 232 / 255, green: 76 / 255, blue: 96 / 255, alpha: 1.0)
+        static let green = UIColor(red: 17 / 255, green: 137 / 255, blue: 23 / 255, alpha: 1.0)
+    }
+}
