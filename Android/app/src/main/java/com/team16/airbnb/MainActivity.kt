b5630c309771ad723fd070e570963d50f1d02750
hanchang97 <nimok97@gmail.com>
2022-05-24 12:28:34 +0900

    feat: bottomNavigationView 배치

--- Android/app/src/main/java/com/team16/airbnb/MainActivity.kt
+++ Android/app/src/main/java/com/team16/airbnb/MainActivity.kt
@@ -2,10 +2,16 @@ package com.team16.airbnb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.team16.airbnb.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
