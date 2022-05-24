adf65441c639f9158f2556199fe1bf24c6fb0369
bibi <bibi6666667@kakao.com>
2022-05-24 15:05:06 +0900

    [#1] Feat: 프로젝트 생성 - Swift Package Manager로 RxSwift, Alamofire 패키지 등록 - 새 프로젝트 생성

---
+++ airbnb/airbnb/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  airbnb
+//
+//  Created by Bibi on 2022/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+    
+    
+}
+
