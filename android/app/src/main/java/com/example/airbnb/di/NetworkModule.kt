0462bc7e59a4dc3715175969ac2902f8e8a1eab5
junseokseo <junseoks9306@gmail.com>
2022-05-24 21:29:31 +0900

    feat: homeContentApi 모듈 생성 (#8)

--- android/app/src/main/java/com/example/airbnb/di/NetworkModule.kt
+++ android/app/src/main/java/com/example/airbnb/di/NetworkModule.kt
@@ -1,11 +1,18 @@
 package com.example.airbnb.di
 
+import com.example.airbnb.network.HomeApi
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.UnstableDefault
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
 import javax.inject.Named
 import javax.inject.Singleton
 
@@ -27,4 +34,18 @@ object NetworkModule {
             .build()
     }
 
+    @OptIn(UnstableDefault::class)
+    @Provides
+    @Singleton
+    fun provideHomeContentsApi(
+        @Named("home") okHttpClient: OkHttpClient
+    ): HomeApi {
+        val contentType = "application/json".toMediaType()
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(Json.asConverterFactory(contentType))
+            .build()
+            .create(HomeApi::class.java)
+    }
 }
--- android/app/src/main/java/com/example/airbnb/network/HomeApi.kt
+++ android/app/src/main/java/com/example/airbnb/network/HomeApi.kt
@@ -1,10 +1,11 @@
 package com.example.airbnb.network
 
+import com.example.airbnb.dto.HomeContentDTO
 import retrofit2.http.GET
 
 interface HomeApi {
 
     @GET("/api/nearMeCities")
-    suspend fun getHomeContents()
+    suspend fun getHomeContents() : HomeContentDTO
 
 }
