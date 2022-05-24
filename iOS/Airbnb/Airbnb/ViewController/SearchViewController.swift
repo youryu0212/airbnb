37c2f8b51d0d30a159813dd4404eaebf8ee2168d
Jason <ggj00125@gmail.com>
2022-05-24 16:34:31 +0900

    [#1]feat :sparkles::SearchViewController class 생성

--- iOS/Airbnb/Airbnb/ViewController/SearchViewController.swift
+++ iOS/Airbnb/Airbnb/ViewController/SearchViewController.swift
@@ -7,6 +7,13 @@
 
 import UIKit
 
-class SearchViewController:UIViewController {
+class SearchViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
     
 }
