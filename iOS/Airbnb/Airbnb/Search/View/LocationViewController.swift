57ba3520f8a172410e8d2e5b85ce418e3dbedf15
Damagucci-Juice <jsr9874@gmail.com>
2022-05-25 01:38:53 +0900

    [#6] feat: Search bar 와 Search Controller 의 깊은 이해를 수반한 작업

--- iOS/Airbnb/Airbnb/Search/View/LocationViewController.swift
+++ iOS/Airbnb/Airbnb/Search/View/LocationViewController.swift
@@ -9,8 +9,61 @@ import UIKit
 
 class LocationViewController: UIViewController {
 
+    private let searchController: UISearchController = {
+        let searchController = UISearchController(searchResultsController: nil)
+        let searchBar = searchController.searchBar
+        searchBar.placeholder = "어디로 여행가세요?"
+        searchBar.searchTextField.clearButtonMode = .never
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.automaticallyShowsCancelButton = false
+        return searchController
+    }()
+        
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemGray3
+        view.backgroundColor = .white
+        navAppearance()
+        setUpSearchController()
+    }
+    
+    private func setUpSearchController() {
+        self.navigationItem.searchController = searchController
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+    }
+}
+
+
+extension LocationViewController {
+    private func navAppearance() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .yellow
+        navigationItem.standardAppearance = appearance
+        navigationItem.scrollEdgeAppearance = appearance
+    }
+}
+
+extension LocationViewController: UISearchResultsUpdating, UISearchBarDelegate {
+    func updateSearchResults(for searchController: UISearchController) {
+        
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if !searchText.isEmpty {
+            let removeButton = UIBarButtonItem(title: "지우기", style: .done, target: nil, action: nil)
+            removeButton.tintColor = .gray
+            self.navigationItem.setRightBarButton(removeButton, animated: false)
+        } else {
+            self.navigationItem.rightBarButtonItem = nil
+        }
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        // 글자 입력수 제한시 사용
+        print(range)
+        print(text)
+        return true
     }
 }
