e6926c2df23b6515303b2ffc0802a13a919e5810
jindonyy <rsefa94@gmail.com>
2022-05-26 04:32:54 +0900

    Design: GNB 구현

--- src/components/Header/GNB.tsx
+++ src/components/Header/GNB.tsx
@@ -1,24 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Container, GNBItem } from '@components/Header/GNB.style';
 
 const GNB_ITEMS = ['숙소', '요금', '인원'];
 
 function GNB() {
-  const [selecetedNavIdx, setSelectedNavIdx] = useState(0);
-
-  const handleNavClick = (idx: number) => {
-    setSelectedNavIdx(idx);
-  };
-
   return (
     <Container>
       {GNB_ITEMS.map((item, idx) => (
-        <GNBItem
-          key={idx}
-          to="/"
-          onClick={() => handleNavClick(idx)}
-          isSelected={selecetedNavIdx === idx}
-        >
+        <GNBItem key={idx} to="/">
           {item}
         </GNBItem>
       ))}
