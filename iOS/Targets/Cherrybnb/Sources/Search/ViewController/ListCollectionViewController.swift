bd43e12d3adbdf7612cb025895808d8fa92d47a4
asqw887 <asqw887@gmail.com>
2022-05-25 12:19:51 +0900

    Feat: 인기여행지 컬렉션 뷰 구현 #6

--- iOS/Targets/Cherrybnb/Sources/Search/ViewController/ListCollectionViewController.swift
+++ iOS/Targets/Cherrybnb/Sources/Search/ViewController/ListCollectionViewController.swift
@@ -9,12 +9,51 @@
 import UIKit
 
 class ListCollectionViewController: UIViewController {
-
+    
+    private var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setCollectionView()
+        setLayout()
+        self.navigationItem.title = "숙소찾기"
     }
     
+    private func setCollectionView(){
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        collectionView = UICollectionView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height), collectionViewLayout: layout)
+        self.view.addSubview(collectionView)
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        collectionView.register(PlaceCell.self, forCellWithReuseIdentifier: "placeCell")
+    }
+    
+    private func setLayout(){
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        collectionView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+}
 
-
+extension ListCollectionViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "placeCell", for: indexPath) as? PlaceCell else { return UICollectionViewCell() }
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let size = CGSize(width: self.view.frame.width, height: 64)
+        return size
+    }
+    
 }
