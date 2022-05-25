9580bea9e05dcd381e517b6aeb6b352e59d8c819
BumgeunSong <isp1195@gmail.com>
2022-05-25 12:13:44 +0900

    Feat: get dummy recommendation through PlaceRecommanding protocol

---
+++ iOS/Targets/Cherrybnb/Sources/Search/Model/DefaultRecommendator.swift
@@ -0,0 +1,24 @@
+//
+//  DefaultRecommendator.swift
+//  Cherrybnb
+//
+//  Created by Bumgeun Song on 2022/05/25.
+//  Copyright © 2022 Codesquad. All rights reserved.
+//
+
+import Foundation
+
+protocol PlaceRecommanding {
+    func recommend(for location: Location, completion: ([Place]?) -> Void)
+}
+
+struct DefaultRecommendator: PlaceRecommanding {
+    
+    let httpService: HTTPService
+    
+    func recommend(for location: Location, completion: ([Place]?) -> Void) {
+        httpService.getRecommendation(for: location) { places in
+            completion(places)
+        }
+    }
+}
