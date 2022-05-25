46173d20df359643e1520f43f590340c6eae521e
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-05-25 11:23:03 +0900

    [#2] Feat: 홈화면 상단에 서치바 추가, 서치바 레이아웃 설정

--- iOS/Airbnb/ViewController.swift
+++ iOS/Airbnb/ViewController.swift
@@ -2,14 +2,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private lazy var searchBar: UISearchBar = {
+        let searchBar:UISearchBar = UISearchBar()
+        searchBar.placeholder = " Search..."
+        searchBar.sizeToFit()
+        searchBar.isTranslucent = false
+        searchBar.searchBarStyle = UISearchBar.Style.prominent
+        searchBar.backgroundImage = UIImage()
+        searchBar.delegate = self
+        return searchBar
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
-        
-        //서치바 추가
-        
+        self.view.backgroundColor = .white
+        setSearchBarLayout()
+    }
+    
+    private func setSearchBarLayout() {
+        self.view.addSubview(searchBar)
+
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
+        searchBar.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        searchBar.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor).isActive = true
     }
 
+}
 
+extension ViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        //검색어 입력 후 데이터 가져오는 로직 필요
+    }
 }
 
