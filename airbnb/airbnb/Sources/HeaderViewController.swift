34ac6a1d3c07b14f8e29db84fa35c3b583b85edd
Jihee hwang <hwangjihee94@gmail.com>
2022-05-25 15:44:03 +0900

    #6 feat: NavigationBar에 SeachBar와 로고 삽입

--- airbnb/airbnb/Sources/HeaderViewController.swift
+++ airbnb/airbnb/Sources/HeaderViewController.swift
@@ -10,10 +10,28 @@ import SnapKit
 
 class HeaderViewController: UIViewController {
     
-    private let underLineView: UIView = {
+    private let navigationBarUnderLineView: UIView = {
         let view = UIView()
         view.backgroundColor = .grey4
         return view
+    }()
+    
+    private let searchController: UISearchController = {
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchBar.placeholder = "어디로 여행가세요?"
+        return searchController
+    }()
+
+    private let logoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "logo")
+        let newSize = CGRect(x: 0, y: 0, width: 90, height: 28)
+        UIGraphicsBeginImageContext(CGSize(width: 90, height: 28))
+        imageView.image?.draw(in: newSize)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()?.withRenderingMode(.alwaysOriginal)
+        UIGraphicsEndImageContext()
+        imageView.image = newImage
+        return imageView
     }()
     
     override func viewDidLoad() {
@@ -25,12 +43,14 @@ class HeaderViewController: UIViewController {
     
     private func configureView() {
         view.backgroundColor = .white
+        navigationItem.searchController = searchController
+        navigationItem.titleView = logoImageView
     }
     
     private func layout() {
-        view.addSubview(underLineView)
+        view.addSubview(navigationBarUnderLineView)
         
-        underLineView.snp.makeConstraints {
+        navigationBarUnderLineView.snp.makeConstraints {
             $0.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
             $0.height.equalTo(1)
         }
