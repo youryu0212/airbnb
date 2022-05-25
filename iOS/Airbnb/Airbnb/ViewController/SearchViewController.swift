a2c2f5babf09c8d98041b5dbbdcc7ac469799d6c
Jason <ggj00125@gmail.com>
2022-05-25 11:52:41 +0900

    [#3]feat :sparkles:: searchBarController로 SearchBar 구현

--- iOS/Airbnb/Airbnb/ViewController/SearchViewController.swift
+++ iOS/Airbnb/Airbnb/ViewController/SearchViewController.swift
@@ -11,9 +11,18 @@ class SearchViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        searchBarController()
     }
     
+    private func searchBarController() {
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchBar.placeholder = "어디로 여행가세요?"
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.automaticallyShowsCancelButton = false
+        searchController.obscuresBackgroundDuringPresentation = false
+        self.navigationItem.searchController = searchController
+        self.navigationItem.title = "숙소 찾기"
+        
+    }
     
 }
