53438a053bc7c8f2a5e0927694a443061eed2068
Senglee Choi <tmdfl1027@gmail.com>
2022-05-25 16:30:40 +0900

    [#16] Feat: 체크인, 체크아웃 날짜 사이에 이용 가능한 숙소 검색 메서드 추가 (#22)
    findByDateBetween() 추가

--- be/src/main/java/kr/codesquad/airbnb/repository/AccommodationRepository.java
+++ be/src/main/java/kr/codesquad/airbnb/repository/AccommodationRepository.java
@@ -1,4 +1,16 @@
 package kr.codesquad.airbnb.repository;
 
-public class AccommodationRepository {
+import kr.codesquad.airbnb.domain.Accommodation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface AccommodationRepository extends JpaRepository {
+
+    List<Accommodation> findByDateBetween(LocalDate checkinDate, LocalDate checkoutDate);
+
+
 }
