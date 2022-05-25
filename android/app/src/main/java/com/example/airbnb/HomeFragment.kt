51177095df4cccc5a7fc646f6db838e9728c5fb4
junseokseo <junseoks9306@gmail.com>
2022-05-25 15:23:07 +0900

    feat: navigation 동작 수행 및 데이터바인딩 적용 (#2)

--- android/app/src/main/java/com/example/airbnb/HomeFragment.kt
+++ android/app/src/main/java/com/example/airbnb/HomeFragment.kt
@@ -4,16 +4,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.airbnb.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tvWhereToTravel.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_searchFragment)
+        }
     }
 
 }
--- android/app/src/main/java/com/example/airbnb/MainActivity.kt
+++ android/app/src/main/java/com/example/airbnb/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.airbnb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import androidx.recyclerview.widget.DiffUtil
 import com.example.airbnb.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,7 +23,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         viewModel.loadHomeContents()
+        setupNav()
     }
 
+    private fun setupNav() {
+        val navController = supportFragmentManager.findFragmentById(R.id.navigation_fragment_main)
+            ?.findNavController()
+        navController?.let { controller ->
+            binding.bottomNavigation.setupWithNavController(controller)
+        }
+
+        navController?.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.homeFragment -> showBottomNav()
+                else -> hideBottomNav()
+            }
+        }
+    }
+
+    private fun showBottomNav() {
+        binding.bottomNavigation.visibility = View.VISIBLE
+    }
+
+    private fun hideBottomNav() {
+        binding.bottomNavigation.visibility = View.GONE
+    }
 
 }
