9369a2d2240b7a225bda0abaafd592515cb5fc1c
sanghyeok-kim <slsnsi1212@gmail.com>
2022-05-25 12:55:40 +0900

    refactor: HeroImageViewCell의 containerStackView, receiveIdeaButton 레이아웃 수정
    - containerStackView에서 receiveIdeaButton을 별도로 분리해서 각각 레이아웃
    
    ref #5

--- iOS/AirbnbApp/AirbnbApp/Source/Present/HeroImageViewCell.swift
+++ iOS/AirbnbApp/AirbnbApp/Source/Present/HeroImageViewCell.swift
@@ -21,8 +21,8 @@ final class HeroImageViewCell: UICollectionViewCell {
     
     private lazy var titleLabel: UILabel = {
         let label = UILabel()
-        label.text = "슬기로운 자연생활"
-        label.lineBreakMode = .byWordWrapping
+        label.text = "슬기로운\n자연생활"
+        label.numberOfLines = 0
         label.font = .SFProDisplay.medium
         label.textColor = .Custom.black
         return label
@@ -30,42 +30,46 @@ final class HeroImageViewCell: UICollectionViewCell {
     
     private lazy var descriptionLabel: UILabel = {
         let label = UILabel()
-        label.text = "메이스가 엄선한 위시리스트를 만나보세요"
-        label.lineBreakMode = .byWordWrapping
-        label.font = .SFProDisplay.regular
-        label.textColor = .Custom.gray3
+        label.text = "에어비앤비가 엄선한\n위시리스트를 만나보세요"
+        label.numberOfLines = 0
+        label.font = .SFProDisplay.regular(17)
+        label.textColor = .Custom.gray1
         return label
-    }()
-    
-    private lazy var receiveIdeaButton: UIButton = {
-        let button = UIButton()
-        button.backgroundColor = UIColor.Custom.gray1
-        button.titleLabel?.text = "여행 아이디어 얻기"
-        button.setTitleColor(UIColor.Custom.white, for: .normal)
-        return button
     }()
     
     private lazy var containerStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .vertical
-        stackView.distribution = .fillEqually
         stackView.spacing = 16
-        
+        stackView.distribution = .equalSpacing
+        [titleLabel, descriptionLabel].forEach {
+            stackView.addArrangedSubview($0)
+        }
         return stackView
+    }()
+    
+    private lazy var receiveIdeaButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = UIColor.Custom.gray1
+        button.setTitle("여행 아이디어 얻기", for: .normal)
+        button.setTitleColor(UIColor.Custom.white, for: .normal)
+        button.layer.cornerRadius = 10
+        var configuration = UIButton.Configuration.bordered()
+        configuration.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 16, bottom: 8, trailing: 16)
+        button.configuration = configuration
+        return button
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         layoutHeroImageView()
         layoutContainerStackView()
+        layoutReceiveIdeaButton()
     }
     
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init with coder is unavailable")
-    }
-    override func prepareForReuse() {
-        super.prepareForReuse()
     }
 }
 
@@ -83,15 +87,22 @@ private extension HeroImageViewCell {
     }
     
     func layoutContainerStackView() {
-        addSubview(containerStackView)
-        
-        containerStackView.addArrangedSubview(titleLabel)
-        containerStackView.addArrangedSubview(descriptionLabel)
-        containerStackView.addArrangedSubview(receiveIdeaButton)
+        heroImageView.addSubview(containerStackView)
         
         containerStackView.snp.makeConstraints { make in
-            make.top.leading.bottom.trailing.equalToSuperview()
-            
+            make.top.equalToSuperview().offset(24)
+            make.leading.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(105)
+        }
+    }
+    
+    func layoutReceiveIdeaButton() {
+        heroImageView.addSubview(receiveIdeaButton)
+        
+        receiveIdeaButton.snp.makeConstraints { make in
+            make.top.equalTo(containerStackView.snp.bottom).offset(16)
+            make.leading.equalToSuperview().offset(16)
+            make.height.equalTo(38)
         }
     }
     
