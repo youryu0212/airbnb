aeb7c95bcba54e58ff466092777da03e174cd83c
junseokseo <junseoks9306@gmail.com>
2022-05-25 14:57:23 +0900

    feat: bottom navigation 구현 (#2)

--- android/app/src/main/java/com/example/airbnb/MainActivity.kt
+++ android/app/src/main/java/com/example/airbnb/MainActivity.kt
@@ -3,16 +3,22 @@ package com.example.airbnb
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import com.example.airbnb.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private val viewModel: ViewModel by viewModels()
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContentView(binding.root)
+
         viewModel.loadHomeContents()
     }
 
