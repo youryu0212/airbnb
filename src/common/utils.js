154d9c08457cd0466789fbb4709511451a0ff974
Nick <75640166+decay95@users.noreply.github.com>
2022-05-25 20:55:08 +0900

    Revert "[TEAM-25] Airbnb 1주차 1번째 PR Nick + Benny (#45)"
    This reverts commit e81f2a6b528d097d884ad4ca9237c31d3beea1f5.

--- src/common/utils.js
+++
@@ -1,6 +0,0 @@
-import { css } from 'styled-components';
-
-export const WrapperMarginCenter = css`
-  width: 1440px;
-  margin: 0 auto;
-`;
--- src/pages/Rooms/Rooms.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Rooms() {
-  return <div>Rooms</div>;
-}
-
-export default Rooms;
