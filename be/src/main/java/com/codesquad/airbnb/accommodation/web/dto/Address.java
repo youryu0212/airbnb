3ec179c5719d4a54307062030d7183abdaa9fc67
seyeong <seyoung7555@naver.com>
2022-05-25 15:18:31 +0900

    feat: 주소 정보를 담을 DTO 객체 생성

---
+++ be/src/main/java/com/codesquad/airbnb/accommodation/web/dto/Address.java
@@ -0,0 +1,37 @@
+package com.codesquad.airbnb.accommodation.web.dto;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Getter
+public class Address {
+    private static final StringBuffer sb = new StringBuffer();
+
+    private String city;
+    private String district;
+    private String street;
+    private String numberOfStreet;
+    private String nameOfBuilding;
+
+    public String toValue() {
+        init();
+
+        StringBuffer result = sb.append(city)
+                .append(" ")
+                .append(district)
+                .append(" ")
+                .append(street)
+                .append(" ")
+                .append(numberOfStreet)
+                .append(" ")
+                .append(nameOfBuilding);
+
+        log.debug("Address: {}", result.toString());
+        return result.toString();
+    }
+
+    private void init() {
+        sb.setLength(0);
+    }
+}
