be37f3efdd635ec28c7114a265f879be26ccff78
bukim0329 <bukim0329@gmail.com>
2022-05-25 10:34:16 +0900

    refactor: 불필요한 코드 삭제

--- src/components/header/DateBox.jsx
+++ src/components/header/DateBox.jsx
@@ -1,15 +1,8 @@
 import React from "react";
 import CheckIn from "./CheckIn";
 import CheckOut from "./CheckOut";
-import CylindricalBox from "./CylindricalBox";
 
 const DateBox = () => {
-    const cylindricalStyle = {
-        width: "360px",
-        height: "76px",
-        border: "",
-    };
-
     return (
         <>
             <CheckIn />
