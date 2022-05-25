d5c245b1d5f8775088d9209435157f96ad7f81a4
jindonyy <rsefa94@gmail.com>
2022-05-26 00:01:59 +0900

    Rename, Design: Header에서 index로 이름 수정 / Header style 적용

---
+++ src/components/Header/index.style.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Container = styled.header`
+  min-height: '640px';
+  padding: '24px 0';
+  background: 'url(./assets/hero-img.jpg) no-repeat center top / cover';
+`;
+
+const Contents = styled.div`
+  max-width: '1440px';
+`;
+
+export { Container, Contents };
