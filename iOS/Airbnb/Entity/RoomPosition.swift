22e39a14b21b40b488b2356ce3e11ec02f94998b
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-05-25 16:38:55 +0900

    [#4] Ref: RoomPosition 내부의 category 속성 타입을 String에서 새로 정의한 RoomPositionCategory 타입으로 수정

--- iOS/Airbnb/Entity/RoomPosition.swift
+++ iOS/Airbnb/Entity/RoomPosition.swift
@@ -3,13 +3,13 @@ import Foundation
 
 struct RoomPosition {
     let address: String
-    private let category: String // 주소가 포함된 시,군,구
+    private let category: RoomPositionCategory // 주소가 포함된 시,군,구
     private let latitude: Float // 소수점 14자리
     private let longitude: Float
     
     init(address: String) {
         self.address = address
-        self.category = "서울시"
+        self.category = RoomPositionCategory(categoryLiteral: "서울시")
         self.latitude = 0.0
         self.longitude = 0.0
     }
