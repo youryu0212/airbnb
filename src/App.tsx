dd3daf6639bc684e7ba72400179b4e1b2cf97349
jindonyy <rsefa94@gmail.com>
2022-05-26 00:03:40 +0900

    Feat: GNB 라우터 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import GlobalStyles from '@/GlobalStyles';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages';
 
 function App() {
-  return <GlobalStyles />;
+  return (
+    <>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
