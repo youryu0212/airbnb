d22f1ffcb3364a8846c4547a8bd0ffb1458c6395
seyeong <seyoung7555@naver.com>
2022-05-25 16:32:57 +0900

    feat: 거리를 계산할 수 있는 util 클래스 생성

---
+++ be/src/main/java/com/codesquad/airbnb/common/CalculatorUtils.java
@@ -0,0 +1,30 @@
+package com.codesquad.airbnb.common;
+
+public class CalculatorUtils {
+    private static final double RADIUS = 6371000;
+    private static final double TO_RADIAN = Math.PI / 180;
+
+    private CalculatorUtils() {
+    }
+
+    /**
+     *
+     * @param latitude1 - base latitude
+     * @param longitude1 - base longitude
+     * @param latitude2 - reference latitude
+     * @param longitude2 - reference longitude
+     * @return distance of two points in meter
+     */
+    public static int calculateDistance(double latitude1, double longitude1, double latitude2, double longitude2) {
+        double deltaLatitude = Math.abs(latitude1 - latitude2) * TO_RADIAN;
+        double deltaLongitude = Math.abs(longitude1 - longitude2) * TO_RADIAN;
+
+        double sinDeltaLatitude = Math.sin(deltaLatitude / 2);
+        double sinDeltaLongitude = Math.sin(deltaLongitude / 2);
+
+        double squareRoot = Math.sqrt(sinDeltaLatitude * sinDeltaLatitude
+                + Math.cos(latitude1 * TO_RADIAN) * Math.cos(latitude2 * TO_RADIAN) * sinDeltaLongitude * sinDeltaLongitude);
+
+        return (int) (2 * RADIUS * Math.asin(squareRoot));
+    }
+}
