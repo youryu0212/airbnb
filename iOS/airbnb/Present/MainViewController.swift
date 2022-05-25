47a64967b5fb86ff546002eaada5e2fe491a148f
sanghee <anshee97@naver.com>
2022-05-25 16:09:05 +0900

    feat: 메인의 스크롤뷰 구현 및 히어로 이미지 추가 (ios-h/airbnb#8)

--- iOS/airbnb/Present/MainViewController.swift
+++ iOS/airbnb/Present/MainViewController.swift
@@ -9,12 +9,33 @@ import UIKit
 
 class MainViewController: UIViewController {
     
+    private let heroImageView: UIImageView = {
+        guard let image = UIImage(named: "img_hero") else {
+            return UIImageView(image: UIImage(named: ""))
+        }
+        let imageView = UIImageView(image: image)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.backgroundColor = .systemMint
+        scrollView.showsVerticalScrollIndicator = false
+        return scrollView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .gray6
         
         setUpSearchController()
+        
+        addViews()
+        setLayout()
     }
     
     private func setUpSearchController() {
@@ -23,5 +44,29 @@ class MainViewController: UIViewController {
         searchController.obscuresBackgroundDuringPresentation = false // true이면 검색 중 뒷 배경 모두 흐릿하게 해줌
         
         self.navigationItem.searchController = searchController
+    }
+    
+    private func addViews() {
+        view.addSubview(scrollView)
+        scrollView.addSubview(heroImageView)
+    }
+    
+    private func setLayout() {
+        scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        
+        heroImageView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor).isActive = true
+        heroImageView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).isActive = true
+        heroImageView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+        heroImageView.heightAnchor.constraint(equalToConstant: 250).isActive = true
+        
+        scrollView.contentLayoutGuide.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor)
+            .isActive = true
+        scrollView.contentLayoutGuide.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+            .isActive = true
+        scrollView.contentLayoutGuide.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        scrollView.contentLayoutGuide.bottomAnchor.constraint(equalTo: heroImageView.bottomAnchor).isActive = true
     }
 }
