1a7417acc90929dd361e8155404ba3e205315559
banjjak2 <interr0bang@naver.com>
2022-05-25 14:50:12 +0900

    feat: 검색 화면이 보일 때 검색칸에 포커스 및 키보드 보이도록 구현

--- android/app/src/main/java/com/example/airbnb/SearchFragment.kt
+++ android/app/src/main/java/com/example/airbnb/SearchFragment.kt
@@ -1,10 +1,11 @@
 package com.example.airbnb
 
+import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.*
-import androidx.databinding.DataBindingUtil
+import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.airbnb.databinding.FragmentSearchBinding
@@ -37,6 +38,14 @@ class SearchFragment : Fragment() {
         }
         registerTextViewForTextChange()
         registerCloseButton()
+        focusInputTextAndShowKeyboard()
+    }
+
+    private fun focusInputTextAndShowKeyboard() {
+        binding.searchInputText.requestFocus()
+
+        val imm = requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(binding.searchInputText, InputMethodManager.SHOW_IMPLICIT)
     }
 
     private fun registerCloseButton() {
