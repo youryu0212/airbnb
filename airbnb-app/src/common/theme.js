401718004953e355d0d488de03a0f5e062b54ba8
moonyerim2 <moonyerim2@gmail.com>
2022-05-25 10:28:20 +0900

    feat: 공통 스타일 작성

---
+++ airbnb-app/src/common/theme.js
@@ -0,0 +1,49 @@
+const color = {
+  white: '#fff',
+  grey1: '#333333',
+  grey2: '#4F4F4F',
+  grey3: '#828282',
+  grey4: '#BDBDBD',
+  grey5: '#E0E0E0',
+  grey6: '#F5F5F7',
+  black: '#010101',
+  primary: '#E84C60',
+  secondary: '#118917',
+};
+
+const fontSize = {
+  logo: '32px',
+  xlarge: '20px',
+  large: '16px',
+  medium: '14px',
+  small: '12px',
+};
+
+const fontWeight = {
+  logo: 900,
+  bold: 700,
+  regular: 400,
+};
+
+const borderRadius = {
+  radius1: '60px',
+  radius2: '40px',
+  radius3: '30px',
+  radius4: '10px',
+};
+
+const modal = {
+  backgourndColor: color.white,
+  borderRadius: borderRadius.radius4,
+  boxShadow: `0px 4px 10px rgba(51, 51, 51, 0.1), 0px 0px 4px rgba(51, 51, 51, 0.05)`,
+};
+
+const theme = {
+  color,
+  fontSize,
+  fontWeight,
+  borderRadius,
+  modal,
+};
+
+export default theme;
