3690ac41e0f18f30779d2c9cbc33f7961b721935
hwj <jminie@naver.com>
2022-05-26 18:27:18 +0900

    feat: 요구사항에 맞춰 달력 구현(오늘 날짜 이전 선택 불가 및 dialongRange로 구현

--- android/app/src/main/java/com/example/airbnb/ui/calendar/CalendarFragment.kt
+++ android/app/src/main/java/com/example/airbnb/ui/calendar/CalendarFragment.kt
@@ -1,19 +1,18 @@
-package com.example.airbnb.ui.wishlist
+package com.example.airbnb.ui.calendar
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.example.airbnb.R
 
-class WishListFragment : Fragment() {
+class CalendarFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_wish_list, container, false)
+        return inflater.inflate(R.layout.fragment_calendar, container, false)
     }
 }
