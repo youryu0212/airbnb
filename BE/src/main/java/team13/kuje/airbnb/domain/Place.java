f8045b657dc311c5d72f41dbe7ef3cce7fb72af0
“kukim” <kukim.dev@gmail.com>
2022-05-26 16:46:26 +0900

    feat: 두 위경도 사이의 거리기반 도착 예정시간 계산 로직 구현
    - 유틸성 객체 MapUtils.distance()의 계산 로직 구현
    - Place.calculateTime() 메소드 구현, 단위 테스트 구현

--- BE/src/main/java/team13/kuje/airbnb/domain/Place.java
+++ BE/src/main/java/team13/kuje/airbnb/domain/Place.java
@@ -7,6 +7,7 @@ import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import team13.kuje.airbnb.util.MapUtils;
 
 @Entity
 @Getter
@@ -14,6 +15,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Place {
 
+	public static final double AVERAGE_SPEED = 80.;
+	public static final int HOUR_TO_MIN = 60;
 	@Id
 	@GeneratedValue
 	private Long id;
@@ -25,40 +28,11 @@ public class Place {
 	private Position position;
 
 
-	public int calculateTime(Position position) {
-		// TODO : 계산로직
+	public int calculateTime(Position inputPosition) {
+		double distance = MapUtils.distance(this.position.getLat(), this.position.getLng(),
+			inputPosition.getLat(), inputPosition.getLng());
 
-		return 0;
-	}
-
-	// http://www.geodatasource.com/developers/
-	private static double distance(double lat1, double lon1, double lat2, double lon2, String unit) {
-
-		double theta = lon1 - lon2;
-		double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
-
-		dist = Math.acos(dist);
-		dist = rad2deg(dist);
-		dist = dist * 60 * 1.1515;
-
-		if (unit == "kilometer") {
-			dist = dist * 1.609344;
-		} else if(unit == "meter"){
-			dist = dist * 1609.344;
-		}
-
-		return (dist);
-	}
-
-
-	// This function converts decimal degrees to radians
-	private static double deg2rad(double deg) {
-		return (deg * Math.PI / 180.0);
-	}
-
-	// This function converts radians to decimal degrees
-	private static double rad2deg(double rad) {
-		return (rad * 180 / Math.PI);
+		return (int) ((distance / AVERAGE_SPEED) * HOUR_TO_MIN);
 	}
 
 }
