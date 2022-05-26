15b0691a3b091aafcf3feae2840ef26dd2fb8f28
Sol <hskim6521@naver.com>
2022-05-26 11:10:52 +0900

    refactor: UIColor Extension 방법 수정
    기존에 사용했던 RGB 값을 하드코딩해서 extension하는 방법에서 Assets의 ColorSet 등록을 이용해 extension하는 방법으로 수정

--- iOS/Airbnb_iOS/Airbnb_iOS/Extension/UIColor+.swift
+++ iOS/Airbnb_iOS/Airbnb_iOS/Extension/UIColor+.swift
@@ -8,12 +8,12 @@
 import UIKit
 
 extension UIColor {
-    static let gray1 = UIColor(red: 51/255, green: 51/255, blue: 51/255, alpha: 1.0)
-    static let gray2 = UIColor(red: 79/255, green: 79/255, blue: 79/255, alpha: 1.0)
-    static let gray3 = UIColor(red: 130/255, green: 130/255, blue: 130/255, alpha: 1.0)
-    static let gray4 = UIColor(red: 189/255, green: 189/255, blue: 189/255, alpha: 1.0)
-    static let gray5 = UIColor(red: 224/255, green: 224/255, blue: 224/255, alpha: 1.0)
-    static let gray6 = UIColor(red: 245/255, green: 245/255, blue: 247/255, alpha: 1.0)
-    static let primary = UIColor(red: 232/255, green: 76/255, blue: 96/255, alpha: 1.0)
-    static let secondary = UIColor(red: 17/255, green: 137/255, blue: 23/255, alpha: 1.0)
+    class var primary: UIColor? { return UIColor(named: "primary") }
+    class var secondary: UIColor? { return UIColor(named: "secondary") }
+    class var gray1: UIColor? { return UIColor(named: "gray1") }
+    class var gray2: UIColor? { return UIColor(named: "gray2") }
+    class var gray3: UIColor? { return UIColor(named: "gray3") }
+    class var gray4: UIColor? { return UIColor(named: "gray4") }
+    class var gray5: UIColor? { return UIColor(named: "gray5") }
+    class var gray6: UIColor? { return UIColor(named: "gray6") }
 }
