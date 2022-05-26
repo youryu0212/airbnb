8ad115064afa0aac0ac59fdba18972f38b545654
hsjang <hsjang0405@gmail.com>
2022-05-26 14:58:46 +0900

    feat: place Controller, Dto 구현
    - GET /api/place 요청 받는 컨트롤러 구현
    - Service 구현중

---
+++ BE/src/main/java/team13/kuje/airbnb/controller/PlaceController.java
@@ -0,0 +1,30 @@
+package team13.kuje.airbnb.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import team13.kuje.airbnb.controller.model.PlaceDto;
+import team13.kuje.airbnb.controller.model.WrapperDto;
+import team13.kuje.airbnb.service.PlaceService;
+
+@RestController
+@RequestMapping("/api/place")
+@RequiredArgsConstructor
+public class PlaceController {
+
+	private final PlaceService placeService;
+
+	@GetMapping
+	public WrapperDto<PlaceDto> findByPosition(
+		@RequestParam(value = "category_tag") String tag,
+		@RequestParam(required = false) String lat,
+		@RequestParam(required = false) String lng
+		) {
+
+		placeService.findByPosition(tag, lat, lng);
+
+	}
+
+}
---
+++ BE/src/main/java/team13/kuje/airbnb/controller/model/PlaceDto.java
@@ -0,0 +1,14 @@
+package team13.kuje.airbnb.controller.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class PlaceDto {
+
+	private String title;
+	private String imageUrl;
+	private String time;
+
+}
