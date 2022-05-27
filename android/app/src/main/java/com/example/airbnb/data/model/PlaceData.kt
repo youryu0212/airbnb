9eecdabb408992f3365d44468d750e186eb5caa5
jminie <79504043+jminie-o8o@users.noreply.github.com>
2022-05-27 14:02:42 +0900

    Revert "[Android] Search화면, 키워드 검색 화면 구현"

--- android/app/src/main/java/com/example/airbnb/data/model/PlaceData.kt
+++
@@ -1,7 +0,0 @@
-package com.example.airbnb.data.model
-
-sealed class PlaceData {
-    data class SearchPlaceData(val placeName: String, val image: String? = null) : PlaceData()
-    data class PopularPlaceData(val placeName: String, val image: String, val distance: String) :
-        PlaceData()
-}
--- android/app/src/main/java/com/example/airbnb/network/dto/HeroImage.kt
+++
@@ -1,8 +0,0 @@
-package com.example.airbnb.network.dto
-
-import com.google.gson.annotations.SerializedName
-
-data class HeroImage(
-    @SerializedName("imageUrl")
-    val imageUrl: String
-)
