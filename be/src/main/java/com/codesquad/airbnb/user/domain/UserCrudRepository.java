13ecf93be725360189b2deea4645385f49051073
seyeong <seyoung7555@naver.com>
2022-06-08 16:25:12 +0900

    feat: User 저장소에 OAuth ID로 조회하는 기능 추가

--- be/src/main/java/com/codesquad/airbnb/user/domain/UserCrudRepository.java
+++ be/src/main/java/com/codesquad/airbnb/user/domain/UserCrudRepository.java
@@ -2,5 +2,9 @@ package com.codesquad.airbnb.user.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserCrudRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByOauthId(String oauthId);
 }
