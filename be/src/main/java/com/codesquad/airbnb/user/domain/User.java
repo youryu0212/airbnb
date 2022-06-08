9c64c812a25871863c185c6d863b688bbc079048
seyeong <seyoung7555@naver.com>
2022-06-08 16:24:58 +0900

    feat: User 도메인에 OAuth 관련 필드 추가

--- be/src/main/java/com/codesquad/airbnb/user/domain/User.java
+++ be/src/main/java/com/codesquad/airbnb/user/domain/User.java
@@ -2,6 +2,7 @@ package com.codesquad.airbnb.user.domain;
 
 import com.codesquad.airbnb.common.BaseTime;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +10,7 @@ import javax.persistence.Id;
 
 import static javax.persistence.GenerationType.IDENTITY;
 
+@NoArgsConstructor
 @Getter
 @Entity
 public class User extends BaseTime {
@@ -18,4 +20,17 @@ public class User extends BaseTime {
     private Long id;
 
     private String username;
+
+    private String oauthId;
+
+    private String token;
+
+    private String email;
+
+    public User(String username, String oauthId, String token, String email) {
+        this.username = username;
+        this.oauthId = oauthId;
+        this.token = token;
+        this.email = email;
+    }
 }
