71033e9c43a8b86bbdf269f3028db200ac0a2642
seyeong <seyoung7555@naver.com>
2022-06-09 11:15:49 +0900

    refactor: jwt 만료기한 등 리팩토링

--- be/src/main/java/com/codesquad/airbnb/user/service/JwtTokenProvider.java
+++ be/src/main/java/com/codesquad/airbnb/user/service/JwtTokenProvider.java
@@ -2,18 +2,31 @@ package com.codesquad.airbnb.user.service;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
 @Component
 public class JwtTokenProvider {
+
+    private static final int DEFAULT_EXPIRE_MINUTE = 30;
+
+    private final String secret;
+
+    public JwtTokenProvider(
+            @Value("${jwt.secret}") String secret) {
+        this.secret = secret;
+    }
+
     public String createAccessToken(String id) {
 
         return JWT.create()
                 .withSubject("jwtToken")
-                .withExpiresAt(new Date(System.currentTimeMillis() + 1800))
+                .withExpiresAt(Date.from(Instant.now().plus(DEFAULT_EXPIRE_MINUTE, ChronoUnit.MINUTES)))
                 .withClaim("id", id)
-                .sign(Algorithm.HMAC256("kim"));
+                .sign(Algorithm.HMAC256(secret));
     }
 }
